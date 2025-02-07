f1e710f27ca2b54ad1850b1fd448782032c34fd1
hedgemouse <lenhardtnimrod@gmail.com>
2025-02-07 19:07:31 +0100

    rendered text, bug fix

--- control.lua
+++ control.lua
@@ -1,33 +1,42 @@
 local afk_mode = false
 
-commands.add_command("start-afk", "Starts the AFK mode", function() --Adds start-afk command
+commands.add_command("start-afk", "Starts the AFK mode", function(event) --Adds start-afk command
     if not afk_mode then --If the AFK mode is off then the following will run, else the one below
+        local player = game.players[event.player_index] -- Get the player who issued the command
+        local player_position = player.position -- Store the player's position in a variable
         game.auto_save("AFK") --Saves the game with the name _autosave-AFK
         game.print("AFK mode started") --Writes a message in the console
         afk_mode = true --Turns on the variable so that it can be used later
+        game.autosave_enabled = true
+        rendering.clear()
+        rendering.draw_text{text = "AFK mode ON", scale_with_zoom = true, surface = player.surface, target = player_position, color = {r = 1, g = 0, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
     else
-        game.print("You need to turn off AFK mode to be able to turn it on") --Writes a message in the console
+        game.print("AFK mode already turned on") --Writes a message in the console
     end
 end)
 
-commands.add_command("stop-afk", "Stops the AFK mode", function() --Adds stop-afk command
+commands.add_command("stop-afk", "Stops the AFK mode", function(event) --Adds stop-afk command
    if afk_mode then --If the AFK mode is on then the following will run, else the one below
+    local player = game.players[event.player_index] -- Get the player who issued the command
+    local player_position = player.position -- Store the player's position in a variable
     game.print("AFK mode stopped") --Writes a message in the console
     afk_mode = false --Turns off the variable
     game.autosave_enabled = true --Turns autosave back
     game.tick_paused = false --Unpauses the game in case it was paused
+    rendering.clear()
+    rendering.draw_text{text = "AFK mode OFF", time_to_live = 300, scale_with_zoom = true, surface = player.surface, target = player_position, color = {r = 0, g = 1, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
    else
-    game.print("You need to turn on AFK mode to be able to turn it off") --Writes a message in the console
+    game.print("AFK mode already turned off") --Writes a message in the console
    end
 end)
 
 script.on_event(defines.events.on_entity_died, function(event)
-    if afk_mode and event.entity then --Check if the AFK mode is on and if the "event" has an entity
+    if afk_mode and event.entity and event.entity.force.name == "player" then --Check if the AFK mode is on, if the "event" has an entity, and if the entity is not in the player faction
         local entity_type = event.entity.prototype.type  --Gets the entity type
         
         --List of defensive entities, which are allowed to be destroyed
         local defensive_entities = {
-            wall = true, gate = true, ["land-mine"] = true, ["ammo-turret"] = true, 
+            wall = true, gate = true, ["ammo-turret"] = true, 
             ["electric-turret"] = true, ["fluid-turret"] = true, ["artillery-turret"] = true, 
             ["construction-robot"] = true, pipe = true, ["pipe-to-ground"] = true, radar = true
         }
@@ -36,6 +45,12 @@ script.on_event(defines.events.on_entity_died, function(event)
             game.print("Game stopped because a non-defensive entity was destroyed")
             game.tick_paused = true --Pauses the game so that the biters dont destroy the base
             game.autosave_enabled = false --Turns off autosave, so that the player can load the last save
+            rendering.clear()
+            rendering.draw_text{text = "Game Paused", scale_with_zoom = true, surface = event.entity.surface, target = event.entity.position, color = {r = 1, g = 1, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
         end
     end
+end)
+
+script.on_load(function()
+    afk_mode = false
 end)
