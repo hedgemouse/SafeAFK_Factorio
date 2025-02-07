410f7aa6ba3ce67a13aa197a38a5dfe6bb8fb43d
hedgemouse <lenhardtnimrod@gmail.com>
2025-02-07 19:14:02 +0100

    control.lua changes

--- control.lua
+++ control.lua
@@ -31,12 +31,14 @@ commands.add_command("stop-afk", "Stops the AFK mode", function(event) --Adds st
 end)
 
 script.on_event(defines.events.on_entity_died, function(event)
+    local player = game.players[event.player_index] -- Get the player who issued the command
+    local player_position = player.position -- Store the player's position in a variable
     if afk_mode and event.entity and event.entity.force.name == "player" then --Check if the AFK mode is on, if the "event" has an entity, and if the entity is not in the player faction
         local entity_type = event.entity.prototype.type  --Gets the entity type
         
         --List of defensive entities, which are allowed to be destroyed
         local defensive_entities = {
-            wall = true, gate = true, ["ammo-turret"] = true, 
+            wall = true, gate = true, ["land-mine"] = true, ["ammo-turret"] = true, 
             ["electric-turret"] = true, ["fluid-turret"] = true, ["artillery-turret"] = true, 
             ["construction-robot"] = true, pipe = true, ["pipe-to-ground"] = true, radar = true
         }
@@ -46,7 +48,7 @@ script.on_event(defines.events.on_entity_died, function(event)
             game.tick_paused = true --Pauses the game so that the biters dont destroy the base
             game.autosave_enabled = false --Turns off autosave, so that the player can load the last save
             rendering.clear()
-            rendering.draw_text{text = "Game Paused", scale_with_zoom = true, surface = event.entity.surface, target = event.entity.position, color = {r = 1, g = 1, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
+            rendering.draw_text{text = "Game Paused", scale_with_zoom = true, surface = event.entity.surface, target = player_position.position, color = {r = 1, g = 1, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
         end
     end
 end)
