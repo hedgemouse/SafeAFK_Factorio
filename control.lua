9a026ed4b68b5d6b4e2456fba86c874fff9d7ee8
hedgemouse <lenhardtnimrod@gmail.com>
2025-02-08 11:10:15 +0100

    Only clears my mods rendered text

--- control.lua
+++ control.lua
@@ -8,7 +8,7 @@ commands.add_command("start-afk", "Starts the AFK mode", function(event) --Adds 
         game.print("AFK mode started") --Writes a message in the console
         afk_mode = true --Turns on the variable so that it can be used later
         game.autosave_enabled = true
-        rendering.clear()
+        rendering.clear("SafeAFK")
         rendering.draw_text{text = "AFK mode ON", scale_with_zoom = true, surface = player.surface, target = player_position, color = {r = 1, g = 0, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
     else
         game.print("AFK mode already turned on") --Writes a message in the console
@@ -23,7 +23,7 @@ commands.add_command("stop-afk", "Stops the AFK mode", function(event) --Adds st
     afk_mode = false --Turns off the variable
     game.autosave_enabled = true --Turns autosave back
     game.tick_paused = false --Unpauses the game in case it was paused
-    rendering.clear()
+    rendering.clear("SafeAFK")
     rendering.draw_text{text = "AFK mode OFF", time_to_live = 300, scale_with_zoom = true, surface = player.surface, target = player_position, color = {r = 0, g = 1, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
    else
     game.print("AFK mode already turned off") --Writes a message in the console
@@ -47,7 +47,7 @@ script.on_event(defines.events.on_entity_died, function(event)
             game.print("Game stopped because a non-defensive entity was destroyed")
             game.tick_paused = true --Pauses the game so that the biters dont destroy the base
             game.autosave_enabled = false --Turns off autosave, so that the player can load the last save
-            rendering.clear()
+            rendering.clear("SafeAFK")
             rendering.draw_text{text = "Game Paused", scale_with_zoom = true, surface = event.entity.surface, target = player_position.position, color = {r = 1, g = 1, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
         end
     end
