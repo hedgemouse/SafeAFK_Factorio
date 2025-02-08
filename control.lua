0935593a352e328ea6722031ef0afbf0f668cce9
hedgemouse <lenhardtnimrod@gmail.com>
2025-02-08 15:05:04 +0100

    Implementing changes suggested by @heinwessels

--- control.lua
+++ control.lua
@@ -1,40 +1,28 @@
-local afk_mode = false
-
 commands.add_command("start-afk", "Starts the AFK mode", function(event) --Adds start-afk command
-    if not afk_mode then --If the AFK mode is off then the following will run, else the one below
-        player = game.players[event.player_index] -- Get the player who issued the command
-        player_position = player.position -- Store the player's position in a variable
-        game.auto_save("AFK") --Saves the game with the name _autosave-AFK
-        game.print("AFK mode started") --Writes a message in the console
-        afk_mode = true --Turns on the variable so that it can be used later
-        game.autosave_enabled = true
-        game.tick_paused = false
-        rendering.clear("SafeAFK")
-        rendering.draw_text{text = "AFK mode ON", scale_with_zoom = true, surface = player.surface, target = player_position, color = {r = 1, g = 0, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
-    else
-        game.tick_paused = false
-        rendering.clear("SafeAFK")
-        game.print("AFK mode already turned on") --Writes a message in the console
-    end
+    player = game.players[event.player_index] -- Get the player who issued the command
+    storage.player_position = player.position -- Store the player's position in a variable
+    game.auto_save("AFK") --Saves the game with the name _autosave-AFK
+    game.print("AFK mode started") --Writes a message in the console
+    storage.afk_mode = true --Turns on the variable so that it can be used later
+    game.autosave_enabled = true
+    game.tick_paused = false
+    rendering.clear("SafeAFK")
+    rendering.draw_text{text = "AFK mode ON", scale_with_zoom = true, surface = player.surface, target = storage.player_position, color = {r = 1, g = 0, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
 end)
 
 commands.add_command("stop-afk", "Stops the AFK mode", function(event) --Adds stop-afk command
-   if afk_mode then --If the AFK mode is on then the following will run, else the one below
+    storage.afk_mode = false --Turns off the variable
+    player = game.players[event.player_index] -- Get the player who issued the command
+    storage.player_position = player.position -- Store the player's position in a variable
     game.print("AFK mode stopped") --Writes a message in the console
-    afk_mode = false --Turns off the variable
     game.autosave_enabled = true --Turns autosave back
     game.tick_paused = false --Unpauses the game in case it was paused
     rendering.clear("SafeAFK")
-    rendering.draw_text{text = "AFK mode OFF", time_to_live = 300, scale_with_zoom = true, surface = player.surface, target = player_position, color = {r = 0, g = 1, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
-   else
-    rendering.clear("SafeAFK")
-    game.print("AFK mode already turned off") --Writes a message in the console
-    game.tick_paused = false
-   end
+    rendering.draw_text{text = "AFK mode OFF", time_to_live = 300, scale_with_zoom = true, surface = player.surface, target = storage.player_position, color = {r = 0, g = 1, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
 end)
 
 script.on_event(defines.events.on_entity_died, function(event)
-    if afk_mode and event.entity and event.entity.force.name == "player" then --Check if the AFK mode is on, if the "event" has an entity, and if the entity is not in the player faction
+    if storage.afk_mode and event.entity and event.entity.force.name == "player" then --Check if the AFK mode is on, if the "event" has an entity, and if the entity is not in the player faction
         local entity_type = event.entity.prototype.type  --Gets the entity type
         
         --List of defensive entities, which are allowed to be destroyed
@@ -49,7 +37,7 @@ script.on_event(defines.events.on_entity_died, function(event)
             game.tick_paused = true --Pauses the game so that the biters dont destroy the base
             game.autosave_enabled = false --Turns off autosave, so that the player can load the last save
             rendering.clear("SafeAFK")
-            rendering.draw_text{text = "Game Paused", scale_with_zoom = true, surface = event.entity.surface, target = player_position, color = {r = 1, g = 1, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
+            rendering.draw_text{text = "Game Paused", scale_with_zoom = true, surface = event.entity.surface, target = storage.player_position, color = {r = 1, g = 1, b = 0, a = 1}, scale = (3)} --Draws a text on the screen
         end
     end
 end)
